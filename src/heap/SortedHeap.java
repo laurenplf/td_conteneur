efb563e5ea0e738bcb81248155532f15ad455c2c
laurenplf <laurent.plfc@gmail.com>
2018-09-21 20:53:50 +0200

    ajout doc

--- src/heap/SortedHeap.java
+++ src/heap/SortedHeap.java
@@ -1,8 +1,9 @@
 package heap;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 
-public class SortedHeap implements Heap<Integer>{
+public class SortedHeap implements Heap<Integer>, Comparator {
 
     protected Integer queue[];
     protected int size;
@@ -26,14 +27,21 @@ public class SortedHeap implements Heap<Integer>{
         return parentPosition;
     }
 
-
+    /** Exchange the two elements whose indices are given as arguments
+     *
+     * @param index1
+     * @param index2
+     */
     protected void exchange(int index1, int index2){
         Integer temp = queue[index1];
         queue[index1] = queue[index2];
         queue[index2] = temp;
     }
 
-
+    /** Put the considered element at the right location in the heap by forcing to go up while possible
+     *
+     * @param index
+     */
     protected void sortElementUp(int index){
         int parentIndex = father(index);
         if (queue[index] > queue[parentIndex]){
@@ -42,7 +50,11 @@ public class SortedHeap implements Heap<Integer>{
         }
     }
 
-
+    /** Insert the considered element at the right location in the heap and update its size
+     *
+     * @param e
+     * @return true if insertion was realized
+     */
     @Override
     public boolean insertElement(Integer e) {
         queue[size] = e;
@@ -53,7 +65,11 @@ public class SortedHeap implements Heap<Integer>{
         return true;
     }
 
-
+    /** Allows user to get the value of the element of the heap with the highest priority
+     *
+     * @return first element of the heap
+     * @throws NoSuchElementException
+     */
     @Override
     public Integer element()
             throws NoSuchElementException{
@@ -65,6 +81,11 @@ public class SortedHeap implements Heap<Integer>{
         }
     }
 
+    /** Find the value of the index of the son with the highest priority (in case of existence)
+     *
+     * @param index
+     * @return index of highest priority son or index given as argument
+     */
     protected int biggestSonIndex(int index){
         int position = index + 1;
         int loga = (int)Math.log(position);
@@ -83,6 +104,10 @@ public class SortedHeap implements Heap<Integer>{
         return biggestSon;
     }
 
+    /** Put the considered element at the right location in the heap by forcing to go down while possible
+     *
+     * @param index
+     */
     protected void sortElementDown(int index){
         int firstSon = biggestSonIndex(index);
         if (queue[firstSon] > queue[index]) {
@@ -91,6 +116,11 @@ public class SortedHeap implements Heap<Integer>{
         }
     }
 
+    /** Remove the first (highest priority) element in the heap, update the heap and its size
+     *
+     * @return first element of the heap
+     * @throws NoSuchElementException
+     */
     @Override
     public Integer popElement()
             throws NoSuchElementException {
@@ -106,11 +136,19 @@ public class SortedHeap implements Heap<Integer>{
         }
     }
 
+    /** Allows user to know if the heap is empty or not
+     *
+     * @return true if the heap is empty
+     */
     @Override
     public boolean isEmpty() {
         return (size == 0);
     }
 
+    /** Allows user to know the size of the heap
+     *
+     * @return size of the heap
+     */
     @Override
     public int size(){
         return this.size;
